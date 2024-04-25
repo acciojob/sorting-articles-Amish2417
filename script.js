3d7293057d0f81f77aae84584fbe0d443a420349
ANISH KUMAR <70019437+Amish2417@users.noreply.github.com>
2024-04-25 06:55:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+let innerhtml = document.querySelector('#band');
+function display(){
+	for(let i = 0; i<bands.length;i++){
+		let x = '';
+		x += <span> bands[i]</span>;		
+	
+}
+	innerhtml.innerHTML = x;
+}
